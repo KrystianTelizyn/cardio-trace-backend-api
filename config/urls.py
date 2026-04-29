46ee9bb7b1ad90308827ccf52685936ce000d61f
KrystianTelizyn <krysian.telizyn@gmail.com>
2026-04-29 11:30:27 +0200

    feat: add measurements app with initial setup and integration into project

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
     path("", include("devices.urls")),
+    path("", include("measurements.urls")),
 ]
